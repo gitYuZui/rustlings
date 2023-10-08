cf148c6fb3673857eec628c41ca0cb4c9e5eb891
Zheng Zezheng <1170941265@qq.com>
2023-10-08 16:37:47 +0000

    临时保存

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -5,9 +5,6 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
-    println!("x has the value {}", x);
+    println!("x has the value {}", 5);
 }
